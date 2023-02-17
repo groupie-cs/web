a264f9efe7be819be3e6ecb9b8a413a748742c2e
Kunwar Sahni <kunwar.sahni01@gmail.com>
2023-02-17 18:01:22 -0500

    UI for rankings

--- components/Account.js
+++ components/Account.js
@@ -116,25 +116,28 @@ export default function Account({ session }) {
                     {loading ? 'Loading ...' : 'Update'}
                 </button>
             </div>
-
+    */}
             <div>
                 <button className={styles.button} onClick={() => supabase.auth.signOut()}>
                     Sign Out
                 </button>
-            </div> */}
+            </div>
 
-            
-            <h1>Your Top Artists</h1>
-            <div className={styles.app}>
-                <div className={styles.hs}>
-                {artistData && artistData.items.map((artist) => {
-                    return (
-                        <div className={styles.card} key={artist.id}>
-                            <h2>{artist.name}</h2>
-                            <img className={styles.artistimg} src={artist.images[1].url} alt={artist.name} />
-                        </div>
-                    )
-                })}
+
+            <div className={styles.center}>
+                <div className={styles.app}>
+                    <h1>Lets look through your tastes</h1>
+                    <div className={styles.hs}>
+                        {artistData && artistData.items.map((artist) => {
+                            return (
+                                <div className={styles.card} key={artist.id}>
+                                    <h2>{artist.name}</h2>
+                                    <img className={styles.artistimg} src={artist.images[0].url} alt={artist.name} />
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <h1>And get your friend's tastes next</h1>
                 </div>
             </div>
 
