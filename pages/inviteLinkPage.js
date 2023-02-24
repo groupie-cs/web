d836e118a80e57abc721877733de8c9cefb66825
Kunwar Sahni <kunwar.sahni01@gmail.com>
2023-02-24 11:35:07 -0500

    Fix Broken Building

--- pages/inviteLinkPage.js
+++ pages/inviteLinkPage.js
@@ -1,31 +1,31 @@
 import React, { useEffect, useState } from 'react';
 
 function InviteLinkPage({ match }) {
-  const inviteLink = match.params.inviteLink;
-  const [landingPageUrl, setLandingPageUrl] = useState('');
+  // const inviteLink = match.params.inviteLink;
+  // const [landingPageUrl, setLandingPageUrl] = useState('');
 
-  useEffect(() => {
-    // Make a call to retrieve the landing page URL associated with the invite link from the backend server
-    const getLandingPageUrl = async () => {
-      try {
-       //TODO: Get invite code from server
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  // useEffect(() => {
+  //   // Make a call to retrieve the landing page URL associated with the invite link from the backend server
+  //   const getLandingPageUrl = async () => {
+  //     try {
+  //      //TODO: Get invite code from server
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
 
-    getLandingPageUrl();
-  }, [inviteLink]);
+  //   getLandingPageUrl();
+  // }, [inviteLink]);
 
-  return (
-    <div>
-      {landingPageUrl ? (
-        <iframe src={landingPageUrl} title="landing page" />
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
+  // return (
+  //   <div>
+  //     {landingPageUrl ? (
+  //       <iframe src={landingPageUrl} title="landing page" />
+  //     ) : (
+  //       <p>Loading...</p>
+  //     )}
+  //   </div>
+  // );
 }
 
 export default InviteLinkPage;
