d7a9b619f5d7547621381347a40af519224c54f9
Kunwar Sahni <kunwar.sahni01@gmail.com>
2023-03-07 16:48:25 -0500

    New Filter UI and Current Location Finding

--- components/ConcertData.js
+++ components/ConcertData.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import styles from '@/styles/Home.module.css'
 import Filter from './filter'
+import DialogSelect from './DialogSelect'
 
 export default function ConcertData({ recData }) {
     return (
         <div className={styles.center}>
             <div className={styles.app}>
                 <h1>Lets look at some concerts</h1>
-                <Filter></Filter>
+                <DialogSelect></DialogSelect>
                 <div className={styles.hs}>
                     {recData && recData._embedded.events.map((rec) => {
                         return (
                             <a target="_blank" rel="noopener noreferrer" className={styles.card} key={rec.id} href={rec.url}>
-                                <h2>{rec.name}</h2>
-                                <img className={styles.artistimg} src={rec.images[0].url} alt={rec.name} />
-                                {/* <h2>${rec.priceRanges[0].min}</h2> */}
+                                {/*Cut rec.name if it is too long */}
+                                <h2>{rec.name.length > 20 ? rec.name.substring(0, 20) + "..." : rec.name}</h2>
+                                <img className={styles.artistimg} src={
+                                    rec.images.sort((a, b) => b.width - a.width)[0].url
+                                } alt={rec.name} />
+                                {rec.priceRanges && <h2>${rec.priceRanges[0].min} - ${rec.priceRanges[0].max}</h2>}
                             </a>
                         )
                     })}
