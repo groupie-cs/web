64f0ff5c70ef93486a3ff2515424a3fce6fbd76f
Kunwar Sahni <kunwar.sahni01@gmail.com>
2023-03-21 14:30:01 -0400

    WIP Home Page

--- components/ConcertData.js
+++ components/ConcertData.js
@@ -1,28 +1,60 @@
 import React from 'react'
 import styles from '@/styles/Home.module.css'
-import Filter from './filter'
 import DialogSelect from './DialogSelect'
 
 export default function ConcertData({ recData }) {
     return (
-        <div className={styles.center}>
-            <div className={styles.app}>
-                <h1>Lets look at some concerts</h1>
-                <DialogSelect></DialogSelect>
-                <div className={styles.hs}>
-                    {recData && recData._embedded.events.map((rec) => {
-                        return (
-                            <a target="_blank" rel="noopener noreferrer" className={styles.card} key={rec.id} href={rec.url}>
-                                {/*Cut rec.name if it is too long */}
-                                <h2>{rec.name.length > 20 ? rec.name.substring(0, 20) + "..." : rec.name}</h2>
-                                <img className={styles.artistimg} src={
+        <div className={styles.homepage}>
+
+            <div className={styles.group}>
+                <h2>Group Members</h2>
+                <div className={styles.groupcard}>
+                    <img className={styles.groupimg} src='https://0.gravatar.com/avatar/f69c5894d3082052322b3126ba59389f?s=400&d=mm' />
+                    <h3 className={styles.cardtext}>Kunwar Sahni</h3>
+                </div>
+
+                <div className={styles.groupcard}>
+                    <img className={styles.groupimg} src='https://0.gravatar.com/avatar/f69c5894d3082052322b3126ba59389f?s=400&d=mm' />
+                    <h3 className={styles.cardtext}>Kunwar Sahni</h3>
+                </div>
+
+                <div className={styles.groupcard}>
+                    <img className={styles.groupimg} src='https://0.gravatar.com/avatar/f69c5894d3082052322b3126ba59389f?s=400&d=mm' />
+                    <h3 className={styles.cardtext}>Kunwar Sahni</h3>
+                </div>
+
+                <div className={styles.groupcard}>
+                    <img className={styles.groupimg} src='https://0.gravatar.com/avatar/f69c5894d3082052322b3126ba59389f?s=400&d=mm' />
+                    <h3 className={styles.cardtext}>Kunwar Sahni</h3>
+                </div>
+            </div>
+
+
+            <div className={styles.concertrecomendations}>
+                <div className={styles.headertitle}>
+                    <h2>Concerts</h2>
+                    <DialogSelect></DialogSelect>
+                </div>
+                {/* <div className={styles.hs}> */}
+                {recData && recData._embedded.events.map((rec) => {
+                    return (
+                        <a target="_blank" rel="noopener noreferrer" className={styles.card} key={rec.id} href={rec.url}>
+                            <div className={styles.concertcard}>
+                                <img className={styles.concertimg} src={
                                     rec.images.sort((a, b) => b.width - a.width)[0].url
                                 } alt={rec.name} />
-                                {rec.priceRanges && <h2>${rec.priceRanges[0].min} - ${rec.priceRanges[0].max}</h2>}
-                            </a>
-                        )
-                    })}
-                </div>
+                                <div className={styles.concertDetails}>
+                                    <h2>{rec.name.length > 30 ? rec.name.substring(0, 30) + "..." : rec.name}</h2>
+                                    <h3>{rec._embedded.venues[0].city.name} - {rec._embedded.venues[0].name}</h3>
+                                    {/* if min price and max price are the same dont display them */}
+                                    
+                                    {rec.priceRanges && <h3>${rec.priceRanges[0].min} - ${rec.priceRanges[0].max}</h3>}
+                                </div>
+                            </div>
+                        </a>
+                    )
+                })}
+                {/* </div> */}
             </div>
         </div>
     )
