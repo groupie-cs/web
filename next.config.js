cb14d8490c8a43c8799da0da1fab20b21fc80cc2
Kunwar Sahni <kunwar.sahni01@gmail.com>
2023-04-22 13:17:01 -0400

    Add Basic PWA Setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = withPWA({
+  // next.js config
+})
